8db4e59ed807c10d014258a4e52df73791584f28
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-31 01:17:10 -0500

    test written for manager fails as expected

---
+++ _tests_/Manager.test.js
@@ -0,0 +1,26 @@
+const { test } = require('jest-circus');
+const Manager = require('../lib/Manager');
+
+const manager = new Manager('Steve', '2468', 'Jaswal1p@yahoo.com', '800888008');
+
+test('test to see if we can get the constructor vlues of manager object', () => {
+    expect(manager.name).toBe('Steve');
+    expect(manager.id).toBe('2468');
+    expect(manager.email).toBe('Jaswal1p@yahoo.com');
+});
+
+test('test to get a name from the getName() method', () => {
+    expect(manager.getName()).toBe('Steve');
+});
+
+test('test to get the id from the getId() method', () => {
+    expect(manager.getId()).toBe('2468');
+});
+
+test('test to get the email from the getEmail() method', () => {
+    expect(manager.getEmail()).toBe('Jaswal1p@yahoo.com');
+});
+
+test('test to get employee role from the getRole() method', () => {
+    expect(manager.getRole()).toBe('Manager');
+});
