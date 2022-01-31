787035b406e33e3bbe17b2be015e1f443d7d345b
Jaswal1p <Jaswal1p@yahoo.com>
2022-01-31 00:21:03 -0500

    first employee test written which failed

---
+++ _tests_/Employee.test.js
@@ -0,0 +1,27 @@
+const { expect } = require('@jest/globals');
+const { test } = require('picomatch');
+const Employee = require('../lib/Employee');
+
+const employee = new Employee('Jaswal', '1357', 'jaswal1p@yahoo.com');
+
+test('test to see if we can get the constructor vlues of employee object', () => {
+    expect(employee.name).toBe('Jaswal');
+    expect(employee.id).toBe('1357');
+    expect(employee.email).toBe('Jaswal1p@yahoo.com');
+});
+
+test('test to get a name from the getName() method', () => {
+    expect(employee.getName()).toBe('Jaswal');
+});
+
+test('test to get the id from the getId() method', () => {
+    expect(employee.getId()).toBe('1357');
+});
+
+test('test to get the email from the getEmail() method', () => {
+    expect(employee.getEmail()).toBe('jaswal1p@yahoo.com');
+});
+
+test('test to get employee role from the getRole() method', () => {
+    expect(employee.getRole()).toBe('Employee');
+});
