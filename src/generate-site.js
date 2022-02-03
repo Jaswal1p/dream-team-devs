0ba4a97a6296030b98b85cdcbb82a67fac0c2f9c
Jaswal1p <Jaswal1p@yahoo.com>
2022-02-03 00:55:17 -0500

    created loop function for choices of emp in team

--- src/generate-site.js
+++ src/generate-site.js
@@ -20,4 +20,55 @@ const generateTeam = (team) => {
         `;
         html.push(managerHtml);
     }
+
+    const generateEngineer = engineer => {
+        console.log(engineer);
+        let engineerHtml = `
+        <div class="card" style="width: 20rem;">
+            <div class="card-header">
+            ${engineer.name} <br/>
+            <i class="fas fa-glasses"></i>Engineer</div>
+            <ul class="list-group list-group-flush">
+             <li class="list-group-item">ID: ${engineer.id}</li>
+             <li class="list-group-item">Email: <span id="email"><a href="mailto:${engineer.email}">${engineer.email}</a></span></li>
+             <li class="list-group-item">Github Username: <a target="_blank" href="https://github.com/${engineer.githubUsername}">${engineer.githubUsername}</a></li>
+            </ul>
+        </div>     
+        `;
+        html.push(engineerHtml);
+    }
+
+    const generateIntern = intern => {
+        console.log(intern);
+        let internHtml = `
+        <div class="card" style="width: 20rem;">
+            <div class="card-header">
+            ${intern.name} <br/>
+            <i class="fas fa-user-graduate"></i>Intern</div>
+            <ul class="list-group list-group-flush">
+             <li class="list-group-item">ID: ${intern.id}</li>
+             <li class="list-group-item">Email: <span id="email"><a href="mailto:${intern.email}">${intern.email}</a></span></li>
+             <li class="list-group-item">School: ${intern.school}</li>
+            </ul>
+        </div>     
+        `;
+        html.push(internHtml);
+    }
+
+    // Now I created loop to go thru all employee choices
+
+    for (let i= 0; i < team.length; i++) {
+        if (team[i].getRole() === "Manager") {
+            generateManager(team[i]);
+        }
+        if (team[i].getRole() === "Engineer") {
+            generateEngineer(team[i]);
+        }
+        if (team[i].getRole() === "Intern") {
+            generateIntern(team[i]);
+        }
+    }
+
+
+
 }
